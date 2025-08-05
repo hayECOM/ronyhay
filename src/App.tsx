9635e0cdc7b98d36196fb7837d02c87c0f99bd8a
Nghiem Xuan Hien Linh <hienlinh1822@gmail.com>
2025-08-05 22:11:45 +0700

    feat: initial base

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
