4d71315c0c9a3e7ddbe7dd2e2772a0652085010a
Nghiem Xuan Hien Linh <hienlinh1822@gmail.com>
2025-08-07 00:06:17 +0700

    feat: test submit form to google sheet connection

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,19 @@ import Ventures from './components/sections/Ventures'
 import Footer from './components/sections/Footer'
 
 function App() {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const url = "https://script.google.com/macros/s/AKfycbzy44HJiPPz8SzpN2dvi4E3ZYDNIOPFqhrT1XND22oTM60I-drpMAVyxoBiExZoGx5d/exec"
+    
+    await fetch(url, {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: JSON.stringify({ Name: e.target.name.value, Email: e.target.email.value })
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(console.error)
+  }
 
   return (
     <Fragment>
@@ -13,6 +26,15 @@ function App() {
       <About />
       <Ventures />
       <Footer />
+
+      <div>
+        <h1 className='text-white'>Test submition</h1>
+        <form onSubmit={handleSubmit}>
+          <input name="name" type='text' placeholder='name' />
+          <input name="email" type='email' placeholder='email' />
+          <button className='text-white' type='submit'>Add</button>
+        </form>
+      </div>
     </Fragment>
   )
 }
